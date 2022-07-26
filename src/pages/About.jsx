09a075ce1acbbe2e266bce998657dc6aeee03d9b
Dimterion <dimterion59@gmail.com>
2022-07-26 16:42:02 +0200

    Started the project

---
+++ src/pages/About.jsx
@@ -0,0 +1,20 @@
+function About() {
+  return (
+    <div>
+      <h1 className="text-6xl mb-4">GitHub Finder</h1>
+      <p className="mb-4 text-2xl font-light">
+        A React app to search GitHub profiles and see profile details. This
+        project is part of the React Front To Back Udemy course by
+        <strong>
+          <a href="https://traversymedia.com"> Brad Traversy</a>
+        </strong>
+        .
+      </p>
+      <p className="text-lg text-gray-400">
+        Version <span className="text-white">1.0.0</span>
+      </p>
+    </div>
+  );
+}
+
+export default About;
