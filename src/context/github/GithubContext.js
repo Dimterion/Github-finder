e665e99d1c58781cceba450eaad929bd4c0031b0
Dimterion <dimterion59@gmail.com>
2022-07-27 16:20:08 +0200

    Added search functionality

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,28 +1,45 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer } from "react";
+import githubReducer from "./GithubReducer";
 
 const GithubContext = createContext();
 
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 
 export const GithubProvider = ({ children }) => {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  const fetchUsers = async () => {
-    const response = await fetch(`${GITHUB_URL}/users`);
-
-    const data = await response.json();
-
-    setUsers(data);
-    setLoading(false);
+  const initialState = {
+    users: [],
+    loading: false,
   };
+
+  const [state, dispatch] = useReducer(githubReducer, initialState);
+
+  // Get search results
+  const searchUsers = async (text) => {
+    setLoading();
+
+    const params = new URLSearchParams({
+      q: text,
+    });
+
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`);
+
+    const { items } = await response.json();
+
+    dispatch({
+      type: "GET_USERS",
+      payload: items,
+    });
+  };
+
+  // Set loading
+  const setLoading = () => dispatch({ type: "SET_LOADING" });
 
   return (
     <GithubContext.Provider
       value={{
-        users,
-        loading,
-        fetchUsers,
+        users: state.users,
+        loading: state.loading,
+        searchUsers,
       }}
     >
       {children}
